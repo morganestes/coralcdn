180b6cd13ec07ec5661214f95373929bf6c63705
Morgan Estes <morgan.estes@gmail.com>
2013-08-26 23:58:03 -0500

    Adding Travis-CI integration.

---
+++ tests/test-unit-tests.php
@@ -0,0 +1,7 @@
+<?php
+
+class WP_CoralCDN_Test_Unit_Tests extends WP_CoralCDN_TestCase {
+    function test_true_is_true() {
+        $this->assertTrue( true ); // Yay, your unit tests are running!
+    }
+}
